10fab1dd5df5bb9847677a258cada663855c3fb6
tamimMuntakimm <130441317+tamimMuntakim@users.noreply.github.com>
2026-01-20 23:28:40 +0600

    redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,13 @@ import {
 } from "react-router";
 import router from './routes/Router.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
-      <ToastContainer/>
+      <ToastContainer />
     </AuthProvider>
   </StrictMode>
 )
